2c7aff19aa7cbdef0da8919d027ed9e465b3608d
Ethan Jin <ejin05@csu.fullerton.edu>
2025-10-03 00:14:09 -0700

    Updated with buttons

--- src/app/Auth/page.tsx
+++ src/app/Auth/page.tsx
@@ -2,8 +2,41 @@
 
 const Auth = () => {
     return (
-        <div className="min-h-screen">
-            Auth Page
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="bg-white shadow-md">
+                <h2 className="text-2xl text-center">
+                    Login
+                </h2>
+
+                <form className="flex flex-col">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="border rounded"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="border rounded"
+                    />
+
+                    <button
+                        type="button"
+                        className=""
+                    >
+                        Login
+                    </button>
+                </form>
+
+                <p className="text-center">
+                    Register Here{" "}
+                    <span className="cursor-pointer">
+                        Sign Up
+                    </span>
+                </p>
+            </div>
         </div>
     )
 }
+
+export default Auth;
